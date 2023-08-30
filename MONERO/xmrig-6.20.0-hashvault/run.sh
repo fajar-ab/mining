2abb7920b207fb6a5d91b58f5c97d12b84acb474
Fajar Fadilah <71520914+fajar-ab@users.noreply.github.com>
2023-08-30 22:21:24 +0700

    Update run.sh

--- MONERO/xmrig-6.20.0-hashvault/run.sh
+++ MONERO/xmrig-6.20.0-hashvault/run.sh
@@ -32,4 +32,4 @@ esac
 
 read -p "port: " port
 
-./xmrig --url pool.hashvault.pro:$port --user 84JW5y1eswJbgL8igw63jM8ekTjxV25BpRcgoHEDEq925VK1cffxuEuHAyBB2xPieijFFt6gcSUTa4w6F9D7KcrJE3L8tyq --pass $user_choice --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./xmrig --url pool.hashvault.pro:$port --user 84JW5y1eswJbgL8igw63jM8ekTjxV25BpRcgoHEDEq925VK1cffxuEuHAyBB2xPieijFFt6gcSUTa4w6F9D7KcrJE3L8tyq --pass $user_choice --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --threads=2
